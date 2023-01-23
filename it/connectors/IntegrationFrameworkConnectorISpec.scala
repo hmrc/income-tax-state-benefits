c5b16be8475345ea8d62979aebb179d035061de7
Nick Gravgaard <nick.gravgaard@digital.hmrc.gov.uk>
2023-01-23 16:18:25 +0000

    SASS-3588 - State benefits for 2023-24 Get API #1938

--- it/connectors/IntegrationFrameworkConnectorISpec.scala
+++ it/connectors/IntegrationFrameworkConnectorISpec.scala
@@ -47,24 +47,47 @@ class IntegrationFrameworkConnectorISpec extends ConnectorIntegrationTest
     s"${taxYear - 1}-${taxYear.toString takeRight 2}"
   }
 
-  ".getAllStateBenefitsData" should {
-    "return correct IF data when correct parameters are passed" in {
-      val httpResponse = HttpResponse(OK, Json.toJson(anAllStateBenefitsData).toString())
+  ".getAllStateBenefitsData" when {
+    "when tax year not 23-24" should {
+      "return correct IF data when correct parameters are passed" in {
+        val httpResponse = HttpResponse(OK, Json.toJson(anAllStateBenefitsData).toString())
 
-      stubGetHttpClientCall(s"/if/income-tax/income/state-benefits/$nino/${toTaxYearParameter(taxYear)}", httpResponse)
+        stubGetHttpClientCall(s"/if/income-tax/income/state-benefits/$nino/${toTaxYearParameter(taxYear)}", httpResponse)
 
-      await(underTest.getAllStateBenefitsData(taxYear, nino)(hc)) shouldBe Right(Some(anAllStateBenefitsData))
+        await(underTest.getAllStateBenefitsData(taxYear, nino)(hc)) shouldBe Right(Some(anAllStateBenefitsData))
+      }
+
+      "return IF error and perform a pagerDutyLog when Left is returned" in {
+        val httpResponse = HttpResponse(INTERNAL_SERVER_ERROR, Json.toJson(SingleErrorBody("some-code", "some-reason")).toString())
+
+        (pagerDutyLoggerService.pagerDutyLog _).expects(*, "GetStateBenefitsResponse")
+
+        stubGetHttpClientCall(s"/if/income-tax/income/state-benefits/$nino/${toTaxYearParameter(taxYear)}", httpResponse)
+
+        await(underTest.getAllStateBenefitsData(taxYear, nino)(hc)) shouldBe
+          Left(ApiError(INTERNAL_SERVER_ERROR, SingleErrorBody("some-code", "some-reason")))
+      }
     }
 
-    "return IF error and perform a pagerDutyLog when Left is returned" in {
-      val httpResponse = HttpResponse(INTERNAL_SERVER_ERROR, Json.toJson(SingleErrorBody("some-code", "some-reason")).toString())
+    "when tax year 23-24" should {
+      "return correct IF data when correct parameters are passed" in {
+        val httpResponse = HttpResponse(OK, Json.toJson(anAllStateBenefitsData).toString())
 
-      (pagerDutyLoggerService.pagerDutyLog _).expects(*, "GetStateBenefitsResponse")
+        stubGetHttpClientCall(s"/if/income-tax/income/state-benefits/23-24/$nino", httpResponse)
 
-      stubGetHttpClientCall(s"/if/income-tax/income/state-benefits/$nino/${toTaxYearParameter(taxYear)}", httpResponse)
+        await(underTest.getAllStateBenefitsData(2024, nino)(hc)) shouldBe Right(Some(anAllStateBenefitsData))
+      }
 
-      await(underTest.getAllStateBenefitsData(taxYear, nino)(hc)) shouldBe
-        Left(ApiError(INTERNAL_SERVER_ERROR, SingleErrorBody("some-code", "some-reason")))
+      "return IF error and perform a pagerDutyLog when Left is returned" in {
+        val httpResponse = HttpResponse(INTERNAL_SERVER_ERROR, Json.toJson(SingleErrorBody("some-code", "some-reason")).toString())
+
+        (pagerDutyLoggerService.pagerDutyLog _).expects(*, "GetStateBenefitsResponse")
+
+        stubGetHttpClientCall(s"/if/income-tax/income/state-benefits/23-24/$nino", httpResponse)
+
+        await(underTest.getAllStateBenefitsData(2024, nino)(hc)) shouldBe
+          Left(ApiError(INTERNAL_SERVER_ERROR, SingleErrorBody("some-code", "some-reason")))
+      }
     }
   }
 
